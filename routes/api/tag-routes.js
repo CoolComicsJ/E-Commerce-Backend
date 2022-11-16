4465990d8469ec28324e519a976111ea4a747e0d
CoolComicsJ <ziggaton@gmail.com>
2022-11-16 00:42:17 -0600

    "Finishing errors"

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -8,7 +8,7 @@ router.get('/', async (req, res) => {
   // be sure to include its associated Product data
   try {
     const TagData = await Tag.findAll({
-      // include: [{ model: Product, ProductTag }],
+      include: [{ model: Product}],
     });
     res.status(200).json(TagData);
   } catch (err) {
@@ -20,8 +20,8 @@ router.get('/:id', async (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
   try {
-    const TagData = await Product.findByPk(req.params.id, {
-      // include: [{ model: Product}],
+    const TagData = await Tag.findByPk(req.params['id'], {
+      include: [{ model: Product, ProductTag}],
     });
 
     if (!TagData) {
